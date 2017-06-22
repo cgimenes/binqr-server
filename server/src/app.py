64cea4fc787c45dafb863fff5f2559ab9588e677
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-06-22 01:25:44 -0300

    Corrigindo infra

--- server/src/app.py
+++ server/src/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Xablauzinho!"
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
