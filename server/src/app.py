2992bc41df6067fe239a40140c1f4218dd56f561
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-06-24 01:03:10 -0300

    Adicionando session

--- server/src/app.py
+++ server/src/app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, session, request
 app = Flask(__name__)
+app.secret_key = 'anachnu tov'
 
 
 @app.route("/")
-def hello():
-    return "Xablauzinho!"
+def index():
+    session['esse_eh_o_meu_parana'] = 'boa noite brasil'
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
