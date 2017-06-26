786779daaf922db83539338dd365db61a7393f99
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-06-25 23:31:22 -0300

    Testando algumas ideias

--- server/src/app.py
+++ server/src/app.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session
 app = Flask(__name__)
 app.secret_key = 'anachnu tov'
 
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def index():
     session['esse_eh_o_meu_parana'] = 'boa noite brasil'
-    return render_template('index.html')
+    return render_template('index.html', vai='uso ou não uso a famigerada session?')
 
+
+@app.route("/process", methods=['POST'])
+def process():
+    return
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
