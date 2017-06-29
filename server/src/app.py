3dd6da2e7ec8c02362afc17315d476fdf97229c4
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-06-29 02:05:23 -0300

    Adicionando rotas

--- server/src/app.py
+++ server/src/app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect, url_for
+from uuid import uuid4
 app = Flask(__name__)
 app.secret_key = 'anachnu tov'
 
 
 @app.route("/", methods=['GET'])
 def index():
-    session['esse_eh_o_meu_parana'] = 'boa noite brasil'
-    return render_template('index.html', vai='uso ou não uso a famigerada session?')
+    if 'uid' not in session:
+        session['uid'] = uuid4()
+    return render_template('index.html', vai=session['uid'])
+
+
+@app.route("/success", methods=['GET'])
+def success():
+    return render_template('success.html', vai=session['uid'])
+
+
+@app.route("/error", methods=['GET'])
+def error():
+    return render_template('error.html', vai=session['uid'])
 
 
 @app.route("/process", methods=['POST'])
 def process():
-    return
+    return redirect(url_for('success'))
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
+    
+# https://stackoverflow.com/questions/31542243/redirect-to-other-view-after-submitting-form
