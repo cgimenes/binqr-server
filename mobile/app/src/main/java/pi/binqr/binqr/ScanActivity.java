04865869393663be10de98ae780ea1c4beb36941
cgimenes <marcelo.gimenes.oliveira@gmail.com>
2017-07-11 23:59:25 -0300

    Revert "Migrando para Kotlin"
    This reverts commit c2cb514c4c50053139f2aedd6c6dc0db71eae540.

---
+++ mobile/app/src/main/java/pi/binqr/binqr/ScanActivity.java
@@ -0,0 +1,129 @@
+package pi.binqr.binqr;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.*;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.ResultPoint;
+import com.journeyapps.barcodescanner.*;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.UnsupportedEncodingException;
+import java.util.*;
+
+/**
+ * This sample performs continuous scanning, displaying the barcode and source image whenever
+ * a barcode is scanned.
+ */
+public class ScanActivity extends AppCompatActivity {
+    private DecoratedBarcodeView barcodeView;
+    private Set<String> scannedParts;
+
+    private BarcodeCallback callback = new BarcodeCallback() {
+        @Override
+        public void barcodeResult(BarcodeResult result) {
+            if(result.getText() == null || scannedParts.contains(result.getText())) {
+                // Prevent duplicate scans
+                return;
+            }
+
+            byte[] rawBytes = new byte[0];
+            try {
+                rawBytes = result.getText().getBytes("ISO-8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+
+            scannedParts.add(result.getText());
+            if (true) {
+                save(rawBytes);
+            }
+        }
+
+
+        @Override
+        public void possibleResultPoints(List<ResultPoint> resultPoints) {
+        }
+    };
+
+    protected void save(byte[]... bytes) {
+        Intent intent = new Intent(this, SaveActivity.class);
+        intent.putExtra("file", bytes);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_scan);
+
+        checkAndAskForPermissions();
+
+        barcodeView = (DecoratedBarcodeView) findViewById(R.id.barcode_scanner);
+        DecoderFactory decoderFactory = new DefaultDecoderFactory(EnumSet.of(BarcodeFormat.QR_CODE), null, "ISO-8859-1", false);
+        barcodeView.getBarcodeView().setDecoderFactory(decoderFactory);
+        barcodeView.decodeContinuous(callback);
+
+        scannedParts = new HashSet<>();
+
+        LinearLayout progress = (LinearLayout) findViewById(R.id.progress);
+
+        for (int i = 1; i < 6; i++) {
+            CheckBox checkBox = new CheckBox(this);
+            checkBox.setText(String.format(Locale.getDefault(), "%d", i));
+            checkBox.setClickable(false);
+            progress.addView(checkBox);
+        };
+
+        TextView first_scan_text = (TextView) findViewById(R.id.first_scan_text);
+        first_scan_text.setVisibility(View.GONE);
+    }
+
+    private void checkAndAskForPermissions() {
+        String[] neededPermissions = new String[]{
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        };
+        Set<String> ungrantedPermissions = new HashSet<>();
+
+        for (String neededPermission : neededPermissions) {
+            if (ContextCompat.checkSelfPermission(this, neededPermission) != PackageManager.PERMISSION_GRANTED) {
+                ungrantedPermissions.add(neededPermission);
+            }
+        }
+
+        if (ungrantedPermissions.size() > 0) {
+            ActivityCompat.requestPermissions(this, ungrantedPermissions.toArray(new String[]{}), 0);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        barcodeView.resume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        barcodeView.pause();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        return barcodeView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
+    }
+}
